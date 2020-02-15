9994d7d5d33ad834914aeae664f758da9c797ae8
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-02-15 11:27:16 +0100

    IronGenerate working with users and mongo working

---
+++ travel/bin/seeds.js
@@ -0,0 +1,51 @@
+// Seeds file that remove all users and create 2 new users
+
+// To execute this seed, run from the root of the project
+// $ node bin/seeds.js
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+const bcryptSalt = 10;
+
+mongoose
+  .connect('mongodb://localhost/travel', {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+let users = [{
+    username: "Arturo",
+    password: bcrypt.hashSync("123", bcrypt.genSaltSync(bcryptSalt)),
+    email: 'arturo@gmail.com'
+  },
+  {
+    username: "Jose",
+    password: bcrypt.hashSync("123", bcrypt.genSaltSync(bcryptSalt)),
+    email: 'jose@gmail.com'
+
+  }
+]
+
+User.deleteMany()
+  .then(() => {
+    return User.create(users)
+  })
+  .then(usersCreated => {
+    console.log(`${usersCreated.length} users created with the following id:`);
+    console.log(usersCreated.map(u => u._id));
+  })
+  .then(() => {
+    // Close properly the connection to Mongoose
+    mongoose.disconnect()
+  })
+  .catch(err => {
+    mongoose.disconnect()
+    throw err
+  })
